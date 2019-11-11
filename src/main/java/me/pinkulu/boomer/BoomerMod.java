038a6a3b2f7a12aa21a9948ee125fc8d31ee0403
asbyth <cyronize@gmail.com>
2019-11-11 09:45:40 -0500

    made the mod work

--- src/main/java/me/pinkulu/boomer/BoomerMod.java
+++ src/main/java/me/pinkulu/boomer/BoomerMod.java
@@ -1,42 +1,32 @@
 package me.pinkulu.boomer;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.SoundManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
-import net.minecraftforge.client.event.sound.SoundEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-
-
-
-
 @Mod(modid = BoomerMod.MODID, version = BoomerMod.VERSION, name = BoomerMod.NAME, acceptedMinecraftVersions = BoomerMod.acceptedMineshaftVersions)
-
 public class BoomerMod {
 
     static final String MODID = "boomer";
-    static final String VERSION ="1.0";
+    static final String VERSION = "1.0";
     static final String NAME = "BoomerMod";
     static final String acceptedMineshaftVersions = "[1.8.9]";
-    private Object ITextComponent;
+    private ResourceLocation soundLocation = new ResourceLocation("boomer", "boomer");
 
     @Mod.EventHandler
     public void onInitialization(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    ResourceLocation s = new ResourceLocation("boomer", "boomer");
-    SoundEvent event = new SoundEvent();
-
     @SubscribeEvent
     public void onChatReceived(ClientChatReceivedEvent e) {
         String msg = e.message.getUnformattedText();
         if (msg.startsWith("You died")) {
-            Minecraft.getMinecraft().thePlayer.playSound("boomer:boomer", 5.0f, 0.5f);
+            Minecraft.getMinecraft().thePlayer.playSound(soundLocation.toString(), 1.0f, 1.0f);
         }
     }
 }
