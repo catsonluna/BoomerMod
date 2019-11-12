820a0d0140ba912b1d439d200fe4df4e0dbbc5f4
pinkulu <kristersj7@gmail.com>
2019-11-12 16:39:43 +0200

    added Commands.java

---
+++ src/main/java/me/pinkulu/boomer/Commands.java
@@ -0,0 +1,25 @@
+package me.pinkulu.boomer;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+
+
+public class Commands extends CommandBase {
+
+    @Override
+    public String getCommandName() {
+        return null;
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender iCommandSender) {
+        return null;
+    }
+
+    @Override
+    public void processCommand(ICommandSender iCommandSender, String[] strings) throws CommandException {
+
+    }
+}
