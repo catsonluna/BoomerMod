bb08c1bbce5f06ed6ffb6ced88fd59356e9e9c98
pinkulu <kristersj7@gmail.com>
2019-11-12 16:32:07 +0200

    Removed a word for Bedwars:finalkill to work

--- src/main/java/me/pinkulu/boomer/BoomerMod.java
+++ src/main/java/me/pinkulu/boomer/BoomerMod.java
@@ -12,10 +12,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class BoomerMod {
 
     static final String MODID = "boomer";
-    static final String VERSION = "1.0";
+    static final String VERSION = "1.0.2";
     static final String NAME = "BoomerMod";
     static final String acceptedMineshaftVersions = "[1.8.9]";
     private ResourceLocation soundLocation = new ResourceLocation("boomer", "boomer");
+
 
 
     @Mod.EventHandler
@@ -26,14 +27,15 @@ public class BoomerMod {
     @SubscribeEvent
     public void onChatReceived(ClientChatReceivedEvent e) {
         String msg = e.message.getUnformattedText();
-        //SkyWars
+            //SkyWars
         if (msg.startsWith("You died")) {
             Minecraft.getMinecraft().thePlayer.playSound(soundLocation.toString(), 1.0f, 1.0f);
         }else if (msg.startsWith("+") && msg.contains("Kill") && msg.contains("coins")) {
             Minecraft.getMinecraft().thePlayer.playSound(soundLocation.toString(), 1.0f, 1.0f);
-        //BedWars
-        }else if (msg.endsWith("FINAL KILL") && msg.contains("by")) {
+            //BedWars
+        }else if (msg.endsWith("FINAL KILL")) {
             Minecraft.getMinecraft().thePlayer.playSound(soundLocation.toString(), 1.0f, 1.0f);
+            //Survival
         }
     }
 }
