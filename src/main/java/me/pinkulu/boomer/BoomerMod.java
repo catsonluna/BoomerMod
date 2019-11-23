e4defc95fb31dcd7deebaedc069137943be67477
pinkulu <kristersj7@gmail.com>
2019-11-23 12:39:48 +0200

    Fix

--- src/main/java/me/pinkulu/boomer/BoomerMod.java
+++ src/main/java/me/pinkulu/boomer/BoomerMod.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class BoomerMod {
 
     static final String MODID = "boomer";
-    static final String VERSION = "1.0.2";
+    static final String VERSION = "1.1";
     static final String NAME = "BoomerMod";
     static final String acceptedMineshaftVersions = "[1.8.9]";
     private ResourceLocation soundLocation = new ResourceLocation("boomer", "boomer");
